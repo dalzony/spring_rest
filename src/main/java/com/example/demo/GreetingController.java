6a754f96aa3fecaa8db9ccd7d2bad544d4d3c121
Minsun Lee <dalzony@gmail.com>
2021-10-21 00:10:34 +0900

    add error

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -10,10 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class GreetingController {
 
 	private static final String template = "Hello-, %s!";
-	private final AtomicLong counter = new AtomicLong();aa
+	private final AtomicLong counter = new AtomicLong();
+
 
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        throw Exception;
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 }

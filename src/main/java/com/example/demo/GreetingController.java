85b9784016e1371fa67d3f9e62ae4fc956c20fa9
Minsun Lee <dalzony@gmail.com>
2021-10-21 00:13:47 +0900

    xxx

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -15,7 +15,11 @@ public class GreetingController {
 
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        throw Exception;
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+
+    @GetMapping("/greeting2")
+	public Greeting greeting2(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));asdf
 	}
 }
